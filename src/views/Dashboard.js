1545369f017ee8b378f942fa44136c367a53cf99
n3ur0mancer <46748400+n3ur0mancer@users.noreply.github.com>
2023-06-12 23:37:14 +0200

    Created simple grid for graph widgets

--- src/views/Dashboard.js
+++ src/views/Dashboard.js
@@ -4,7 +4,16 @@ import "@fontsource/hanken-grotesk";
 import "./Dashboard.css";
 
 function Dashboard() {
-  return <div></div>;
+  return (
+    <div>
+      <div className="dashboard-main-grid-container">
+        <div className="dashboard-big-square-grid-container"></div>
+        <div className="dashboard-rectangle-grid-container"></div>
+        <div className="dashboard-small-square-grid-container-left"></div>
+        <div className="dashboard-small-square-grid-container-right"></div>
+      </div>
+    </div>
+  );
 }
 
 export default Dashboard;
