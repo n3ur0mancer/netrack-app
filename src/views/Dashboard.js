ff367c99b423c8961d10e88c07bf1e7547731f73
n3ur0mancer <46748400+n3ur0mancer@users.noreply.github.com>
2023-06-13 15:29:02 +0200

    Applied basic styling to graphs

--- src/views/Dashboard.js
+++ src/views/Dashboard.js
@@ -17,19 +17,51 @@ function Dashboard() {
                 type: "scatter",
                 x: [1, 2, 3, 4, 5, 6],
                 y: [30134, 31400, 30532, 29748, 26340, 25534],
-                marker: { color: "blue" },
+                marker: { color: "white" },
               },
               {
                 name: "Stocks",
                 type: "scatter",
                 x: [1, 2, 3, 4, 5, 6],
                 y: [19643.36, 20044.8, 20268, 20381.4, 20692.6, 21034.2],
-                marker: { color: "green" },
+                marker: { color: "white" },
               },
             ]}
             style={{ width: "100%", height: "100%" }}
             config={{ responsive: true }}
-            layout={{ title: "All assets" }}
+            layout={{
+              title: {
+                text: "All assets",
+                font: {
+                  family: "Hanken Grotesk",
+                  color: "white",
+                  size: 25,
+                },
+                xref: "x",
+                x: 0.08,
+              },
+              plot_bgcolor: "#262626",
+              paper_bgcolor: "#262626",
+              yaxis: {
+                tickfont: {
+                  amily: "Hanken Grotesk",
+                  color: "white",
+                },
+                showgrid: false,
+              },
+              xaxis: {
+                tickfont: {
+                  amily: "Hanken Grotesk",
+                  color: "white",
+                },
+                showgrid: false,
+              },
+              legend: {
+                font: {
+                  color: "white",
+                },
+              },
+            }}
           />
         </div>
         <div className="dashboard-rectangle-grid-container">
@@ -40,44 +72,123 @@ function Dashboard() {
                 type: "scatter",
                 x: [1, 2, 3, 4, 5, 6],
                 y: [30134, 31400, 30532, 29748, 26340, 25534],
-                marker: { color: "blue" },
+                marker: { color: "#262626" },
               },
             ]}
             style={{ width: "100%", height: "100%" }}
             config={{ responsive: true }}
-            layout={{ title: "Complete Networth" }}
+            layout={{
+              title: {
+                text: "Overall net worth",
+                font: {
+                  family: "Hanken Grotesk",
+                  color: "#262626",
+                  size: 25,
+                },
+                xref: "x",
+                x: 0.08,
+              },
+              yaxis: {
+                tickfont: {
+                  amily: "Hanken Grotesk",
+                  color: "#262626",
+                },
+                showgrid: false,
+              },
+              xaxis: {
+                tickfont: {
+                  amily: "Hanken Grotesk",
+                  color: "#262626",
+                },
+                showgrid: false,
+              },
+            }}
           />
         </div>
         <div className="dashboard-small-square-grid-container-left">
           <Plot
             data={[
               {
-                name: "Winner",
+                name: "Losing",
                 type: "scatter",
                 x: [1, 2, 3, 4, 5, 6],
-                y: [19643.36, 20044.8, 20268, 20381.4, 20692.6, 21034.2],
-                marker: { color: "green" },
+                y: [30134, 31400, 30532, 29748, 26340, 25534],
+                marker: { color: "white" },
               },
             ]}
             style={{ width: "100%", height: "100%" }}
             config={{ responsive: true }}
-            layout={{ title: "Winner" }}
+            layout={{
+              title: {
+                text: "Losing",
+                font: {
+                  family: "Hanken Grotesk",
+                  color: "white",
+                  size: 25,
+                },
+                xref: "x",
+                x: 0.15,
+              },
+              plot_bgcolor: "#B286FD",
+              paper_bgcolor: "#B286FD",
+              yaxis: {
+                tickfont: {
+                  amily: "Hanken Grotesk",
+                  color: "white",
+                },
+                showgrid: false,
+              },
+              xaxis: {
+                tickfont: {
+                  amily: "Hanken Grotesk",
+                  color: "white",
+                },
+                showgrid: false,
+              },
+            }}
           />
         </div>
         <div className="dashboard-small-square-grid-container-right">
           <Plot
             data={[
               {
-                name: "Loser",
+                name: "Winning",
                 type: "scatter",
                 x: [1, 2, 3, 4, 5, 6],
-                y: [30134, 31400, 30532, 29748, 26340, 25534],
-                marker: { color: "red" },
+                y: [19643.36, 20044.8, 20268, 20381.4, 20692.6, 21034.2],
+                marker: { color: "#262626" },
               },
             ]}
             style={{ width: "100%", height: "100%" }}
             config={{ responsive: true }}
-            layout={{ title: "Loser" }}
+            layout={{
+              title: {
+                text: "Winning",
+                font: {
+                  family: "Hanken Grotesk",
+                  color: "#262626",
+                  size: 25,
+                },
+                xref: "x",
+                x: 0.15,
+              },
+              yaxis: {
+                tickfont: {
+                  amily: "Hanken Grotesk",
+                  color: "#262626",
+                },
+                showgrid: false,
+              },
+              xaxis: {
+                tickfont: {
+                  amily: "Hanken Grotesk",
+                  color: "#262626",
+                },
+                showgrid: false,
+              },
+              plot_bgcolor: "#B2F042",
+              paper_bgcolor: "#B2F042",
+            }}
           />
         </div>
       </div>
