4f948183e0ed328121c1f32bb74ed544ac4e6095
n3ur0mancer <46748400+n3ur0mancer@users.noreply.github.com>
2023-06-13 14:05:24 +0200

    Integrated first plotly chart

--- src/views/Dashboard.js
+++ src/views/Dashboard.js
@@ -1,14 +1,36 @@
 import React from "react";
+import Plot from "react-plotly.js";
 import Header from "../components/Header/Header";
 import "@fontsource/hanken-grotesk";
-import "./Dashboard.css";
+//import "./Dashboard.css";
 
 function Dashboard() {
   return (
     <div>
       <Header />
       <div className="dashboard-main-grid-container">
-        <div className="dashboard-big-square-grid-container"></div>
+        <div className="dashboard-big-square-grid-container">
+          <Plot
+            data={[
+              {
+                name: "Cash",
+                type: "scatter",
+                x: [1, 2, 3, 4, 5, 6],
+                y: [30134, 31400, 30532, 29748, 26340, 25534],
+                marker: { color: "blue" },
+              },
+              {
+                name: "Stocks",
+                type: "scatter",
+                x: [1, 2, 3, 4, 5, 6],
+                y: [19643.36, 20044.8, 20268, 20381.4, 20692.6, 21034.2],
+                marker: { color: "green" },
+              },
+            ]}
+            style={{ width: "100%", height: "100%" }}
+            config={{ responsive: true }}
+          />
+        </div>
         <div className="dashboard-rectangle-grid-container"></div>
         <div className="dashboard-small-square-grid-container-left"></div>
         <div className="dashboard-small-square-grid-container-right"></div>
