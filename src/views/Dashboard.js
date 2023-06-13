71231da997632808fef39e4c7f6119f687c16c70
n3ur0mancer <46748400+n3ur0mancer@users.noreply.github.com>
2023-06-13 14:28:26 +0200

    Added data placeholder files

--- src/views/Dashboard.js
+++ src/views/Dashboard.js
@@ -2,11 +2,11 @@ import React from "react";
 import Plot from "react-plotly.js";
 import Header from "../components/Header/Header";
 import "@fontsource/hanken-grotesk";
-//import "./Dashboard.css";
+import "./Dashboard.css";
 
 function Dashboard() {
   return (
-    <div>
+    <div className="dashboard-main-container">
       <Header />
       <div className="dashboard-main-grid-container">
         <div className="dashboard-big-square-grid-container">
@@ -31,9 +31,51 @@ function Dashboard() {
             config={{ responsive: true }}
           />
         </div>
-        <div className="dashboard-rectangle-grid-container"></div>
-        <div className="dashboard-small-square-grid-container-left"></div>
-        <div className="dashboard-small-square-grid-container-right"></div>
+        <div className="dashboard-rectangle-grid-container">
+          <Plot
+            data={[
+              {
+                name: "Placeholder",
+                type: "scatter",
+                x: [1, 2, 3, 4, 5, 6],
+                y: [30134, 31400, 30532, 29748, 26340, 25534],
+                marker: { color: "blue" },
+              },
+            ]}
+            style={{ width: "100%", height: "100%" }}
+            config={{ responsive: true }}
+          />
+        </div>
+        <div className="dashboard-small-square-grid-container-left">
+          <Plot
+            data={[
+              {
+                name: "Winner",
+                type: "scatter",
+                x: [1, 2, 3, 4, 5, 6],
+                y: [19643.36, 20044.8, 20268, 20381.4, 20692.6, 21034.2],
+                marker: { color: "green" },
+              },
+            ]}
+            style={{ width: "100%", height: "100%" }}
+            config={{ responsive: true }}
+          />
+        </div>
+        <div className="dashboard-small-square-grid-container-right">
+          <Plot
+            data={[
+              {
+                name: "Loser",
+                type: "scatter",
+                x: [1, 2, 3, 4, 5, 6],
+                y: [30134, 31400, 30532, 29748, 26340, 25534],
+                marker: { color: "red" },
+              },
+            ]}
+            style={{ width: "100%", height: "100%" }}
+            config={{ responsive: true }}
+          />
+        </div>
       </div>
     </div>
   );
